f0b0be8aa04afbf2076e40f57318c9fbe9e217b9
Adrian Sims <adriansims62781@gmail.com>
2018-01-18 19:11:11 +0000

    Add project files.

---
+++ CheckDats/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDats
+{
+    class Dat
+    {
+        public string Name { get; set; }
+        public DateTime FileSystemDate { get; set; }
+        public DateTime InFileDate { get; set; }
+
+    }
+    internal class Program
+    {
+        static void GetRjisZips(ref List<Dat> datlist, string path)
+        {
+            var files = Directory.GetFiles(path, "RJFA*.ZIP");
+            datlist.AddRange(files.Select(x => new Dat { Name = x }));
+            var dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                GetRjisZips(ref datlist, dir);
+            }
+        }
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var datlist = new List<Dat>();
+                GetRjisZips(ref datlist, "s:\\");
+                datlist.ForEach(x => Console.WriteLine($"{x.Name}"));
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add CheckDats/Program.cs
+git add CheckDats/App.config
+git add CheckDats/Properties/AssemblyInfo.cs
+git add CheckDats/CheckDats.csproj
+git add CheckDats.sln
+git add .gitignore
